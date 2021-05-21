63303adc4ec9e639e6623ec4faf07b290d7c44b1
somya <somyasheti@gmail.com>
2021-05-21 10:08:57 +0530

    1st

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const app=express()
 const path = require('path');
@@ -7,11 +6,13 @@ const hbs = require('hbs');
 require("./db/conn")
 const Register = require('./models/registers');
 const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
 
 const PORT=process.env.PORT||3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname,"../public")))
 
@@ -19,13 +20,12 @@ app.set('view engine','hbs');
 app.set('views','../templates/views')
 hbs.registerPartials('../templates/partials')
 
-console.log(process.env.SECRET_KEY);
-
 //create new account of user
 app.post('/login',async(req,res)=>{
     try{
         const pwd=req.body.sign_password;
         const cpwd=req.body.sign_cpassword;
+        const name=req.body.sign_name
         if(pwd===cpwd){
             const registerUser=new Register({
                 name:req.body.sign_name ,
@@ -33,10 +33,16 @@ app.post('/login',async(req,res)=>{
                 Phone:req.body.sign_mobileno ,
                 password:req.body.sign_password
             })
-
             const token=await registerUser.genrateAuthToken();
+            res.cookie("jwt",token)
+            //optional field after token    expires:new Date(Date.now()+300000000)
             const registered=await registerUser.save();
-            res.status(201).render('index')
+            res.status(201).render('index',{
+                login:'Login',
+                register:'Register',
+                login_h:'/login',
+                register_h:'/login'
+            })
         }
     }
     catch(error){
@@ -55,10 +61,20 @@ app.post('/enter',async(req,res)=>{
         
         const isMatch=await bcrypt.compare(pwd,username.password)
         const token=await username.genrateAuthToken();
+
+        res.cookie("jwt",token,{
+            expires:new Date(Date.now()+50000),
+            httpOnly:true
+        });
         const registered=await username.save();
 
         if(isMatch){
-            res.status(201).render('client');
+            res.status(201).render('client',{
+                login:name,
+                register:'Cart',
+                login_h:'/index',
+                register_h:'/cart'
+            });
         }
     }
     catch(err){
@@ -67,7 +83,12 @@ app.post('/enter',async(req,res)=>{
 })
 
 app.get("/",(req,res)=>{
-    res.render('index')
+    res.render('index',{
+        login:'Login',
+        register:'Register',
+        login_h:'/login',
+        register_h:'/login'
+    })
 })
 
 app.get("/login",(req,res)=>{
